d6f1e4175dbd2d703517624f7c390bd774b5bd93
Jose Domenico <domenicodiaz.joseangel@gmail.com>
2022-03-23 00:08:14 -0700

    Pre-Deployment-Vercel

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { ReactChild, ReactFragment, ReactPortal } from 'react'
 
 import { DarkLayout } from '../components/layouts/DarkLayout'
 import { MainLayout } from '../components/layouts/MainLayout'
@@ -22,7 +23,7 @@ export default function AboutPage() {
 }
 
 
-AboutPage.getLayout = function getLayout( page ) {
+AboutPage.getLayout = function getLayout( page: boolean | ReactChild | ReactFragment | ReactPortal | null | undefined ) {
   return (
     <MainLayout>
       <DarkLayout>
